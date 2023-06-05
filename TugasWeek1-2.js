adf7899c3417b3b6d2725e4edfacb4fd71b16bca
Izaaz Waskito <116268166+izaazwaskito@users.noreply.github.com>
2023-06-05 14:05:53 +0700

    TugasWeek1-2.js

--- TugasWeek1-2.js
+++ TugasWeek1-2.js
@@ -1,23 +1,17 @@
 const mtk = 80
-const bahasaIndonesia = 90
+const bahasaIndonesia = "90"
 const bahasaInggris = 89
 const ipa =  69
+const nilai = (mtk+bahasaIndonesia+bahasaInggris+ipa)/4
+ 
+    cekGrade = nilai >= 90 && nilai <=100 ? "Grade = A" :
+    nilai >= 80 && nilai<=89 ? "Grade = B" :
+    nilai >= 70 && nilai<=79 ? "Grade = C" :
+    nilai >= 60 && nilai<=69 ? "Grade = D" :
+    nilai >= 00 && nilai<=59 ? "Grade = E" :
+    "Nilai kurang dari 0 atau lebih dari 100";
 
-if(typeof mtk === "number" && typeof bahasaIndonesia === "number" && typeof bahasaInggris === "number" && typeof ipa === "number"){
-    const nilai = (mtk+bahasaIndonesia+bahasaInggris+ipa)/4
-    console.log("Rata-Rata = "+nilai)
-    if(nilai >= 90 && nilai <=100){
-        console.log("Grade = A");
-    } else if (nilai >= 80 && nilai<=89){
-        console.log("Grade = B");
-    }
-    else if (nilai >= 70 && nilai<=79){
-        console.log("Grade = C");
-    }
-    else if (nilai >= 60 && nilai<=69){
-        console.log("Grade = D");
-    }
-    else if (nilai >= 00 && nilai<=59){
-        console.log("Grade = E");
-    }
-}else{console.log("tipe data ada yang tidak sesuai")};
+    cekVariabel = typeof mtk === "number" && typeof bahasaIndonesia === "number" && typeof bahasaInggris === "number" && typeof ipa === "number" ? console.log(`Rata-Rata = ${nilai} 
+    ${cekGrade}`) : "tipe data ada yang tidak sesuai";
+    console.log(cekVariabel);
+    
